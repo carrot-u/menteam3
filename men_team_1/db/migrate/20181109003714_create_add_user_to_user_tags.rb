dc68fd600ef4f0a7e1ef6900b43652beb61ce5a2
Ken Weed <kenneth.weed@instacart.com>
2018-11-08 17:09:12 -0800

    Removed accidental models

--- men_team_1/db/migrate/20181109003714_create_add_user_to_user_tags.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddUserToUserTags < ActiveRecord::Migration[5.2]
-  def change
-    create_table :add_user_to_user_tags do |t|
-      t.references :userid, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
