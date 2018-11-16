b57bcd9a6a7cf2aa9e755b26c98a83809ae4fc96
Sveta Mohan <sveta.mohan@instacart.com>
2018-11-15 16:34:32 -0800

    Add Factory Bot

---
+++ men_team_1/app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+# class UsersController < ApplicationController
+#
+#   def show
+#     @user = User.find(params[:id])
+#   end
+#
+#   def new
+#     @user = User.new
+#   end
+#
+#   def create
+#     @user = User.new(user_params)
+#     if @user.save
+#       log_in @user
+#       flash[:success] = "Welcome to the Sample App!"
+#       redirect_to @user
+#     else
+#       render 'new'
+#     end
+#   end
+#
+#   def edit
+#     @user = User.find(params[:id])
+#   end
+#
+#   private
+#
+#   def user_params
+#     params.require(:user).permit(:first_name, :last_name, :email) #, :position)
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+  #
+  # def index
+  #   @user = User.new(user_params)
+  #
+  # end
---
+++ men_team_1/spec/factories.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+
+  factory :user do
+    first_name "Joe"
+    last_name "Smith"
+    email "joe@gmail.com"
+    position "Engineer"
+    bio "Hello, my name is Joe and I like to code"
+  end
+end
