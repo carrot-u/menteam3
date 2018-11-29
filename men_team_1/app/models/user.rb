5ee1d2eec322d7d9a7a62fca0de9b8aaaa6c173f
Alex Wong <awong42191@gmail.com>
2018-11-29 15:44:20 -0800

    add name helper method

--- men_team_1/app/models/user.rb
+++ men_team_1/app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
                     uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }
-  
+
   # has_many :pairings
   has_many :mentee_pairings, class_name: 'Pairing', foreign_key: 'mentee_id'
   has_many :mentor_pairings, class_name: 'Pairing', foreign_key: 'mentor_id'
@@ -17,6 +17,12 @@ class User < ApplicationRecord
   # has_many :mentors, class_name: 'User', through: :pairings
   has_many :posts
   has_many :comments
+
+  has_many :topics, through: :user_tags
+
+  def name
+    "#{first_name} #{last_name}"
+  end
 
 end
  
