60cffd4dddc9cf10b8ee9a7c8c682cbb85762a7e
Ken Weed <kenneth.weed@instacart.com>
2018-12-03 00:12:34 -0800

    Added edit and index views and methods, as well as an update and destory method for Posts

--- men_team_1/app/controllers/posts_controller.rb
+++ men_team_1/app/controllers/posts_controller.rb
@@ -1,5 +1,11 @@
 class PostsController < ApplicationController
+  # before_action :user, only: [:index, :new, :create, :edit, :show, :destroy, :update]
+  before_action :set_post, only: [:edit, :show, :destroy, :update]
     
+    def index 
+      @post = Post.all 
+    end
+
   
     def new 
       @post = Post.new
@@ -17,15 +23,44 @@ class PostsController < ApplicationController
     end
 
 
-    def show 
-      @post = Post.find(params[:id])
+    def update
+      if @post.update(post_params)
+        flash[:notice] = "Your question was updated"
+        redirect_to post_path(@post)
+      else
+        render 'edit'
+      end
     end
+    
+
+
+    def show 
+    end
+
+    def edit
+    end
+
+    def destroy
+      @post.destroy
+      flash[:notice] = "Your question has been removed"
+      redirect_to posts_path
+    end
+
 
   
 
     private
+
+      def set_post
+        @post = Post.find(params[:id])
+      end
+
       def post_params
         params.require(:post).permit(:subject, :body)
+      end
+
+      def user
+        @user = User.find(session[:user_id])
       end
 
 
