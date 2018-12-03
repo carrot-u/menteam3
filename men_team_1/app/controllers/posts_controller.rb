8c05384e8a7996e81f9380482613582b7cd0d2a5
Ken Weed <kenneth.weed@instacart.com>
2018-12-02 22:25:35 -0800

    created new, create, and show methods for posts controller and added validations

--- men_team_1/app/controllers/posts_controller.rb
+++ men_team_1/app/controllers/posts_controller.rb
@@ -1,43 +1,33 @@
 class PostsController < ApplicationController
     
-    def index
-      @posts = Post.all
-    end
   
-    def new
-        @post = Post.new
+    def new 
+      @post = Post.new
     end
-  
-    def show
-    end
-  
+
+
     def create
       @post = Post.new(post_params)
-  
-      respond_to do |format|
-        if @post.save
-          format.html { redirect_to @post, notice: 'Post was successfully created.' }
-          format.json { render :show, status: :created, location: @post }
-        else
-          format.html { render :new }
-          format.json { render json: @post.errors, status: :unprocessable_entity }
-        end
+      if @post.save
+        flash[:notice] = "Your question was posted!"
+        redirect_to post_path(@post)
+      else
+        render 'new'
       end
     end
 
-    def user 
-        @user = User.find(session[:user_id])
-    end
-  
-    private
-    def set_post
-        @post = Post.find(params[:id])
+
+    def show 
+      @post = Post.find(params[:id])
     end
 
-    def post_params
-        params.require(:post).permit(:title, :body)
-    end
+  
+
+    private
+      def post_params
+        params.require(:post).permit(:subject, :body)
+      end
+
 
 
 end
-       
--- men_team_1/app/models/post.rb
+++ men_team_1/app/models/post.rb
@@ -1,5 +1,9 @@
 class Post < ApplicationRecord
    ### validates :user_id, presence: true ###
+
+    validates :subject, presence: true, length: {minimum: 3, maximum: 40}
+    validates :body, presence: true, length: {minimum: 10, maximum: 600}
+
     belongs_to :user
     belongs_to :pairing
     has_many :comments
