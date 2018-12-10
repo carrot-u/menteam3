5a09db4266ff6efb73e2a46df1954043aa18a0e1
Sveta Mohan <sveta.mohan@instacart.com>
2018-12-10 08:13:07 -0800

    User-Tags flow and link to Topics

--- men_team_1/app/controllers/user_tags_controller.rb
+++ men_team_1/app/controllers/user_tags_controller.rb
@@ -1,2 +1,34 @@
 class UserTagsController < ApplicationController
+  before_action :user, only: [:index, :new, :create]
+
+  def index
+
+    @user_tags = UserTag.where(user_id: @user.id)
+    
+  end
+
+  def new
+    @user_tags = UserTag.new
+  end
+
+
+  def create
+    # @user_tags = UserTag.create(params[@user.id, :topic_id])
+    @user_tags  = UserTag.new
+    @user_tags.topic_id = params[:user_tag][:topic_id]
+    @user_tags.user_id = @user.id
+    @user_tags.save
+    redirect_to user_tags_path
+  end
+
+
+
+private
+
+  def user
+    @user = User.find(session[:user_id])
+  end
+
+
+
 end
