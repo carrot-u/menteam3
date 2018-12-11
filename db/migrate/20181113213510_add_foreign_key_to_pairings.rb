51d395c34c6a211b8a34722346deb9d6b64a79f4
Alex Wong <awong42191@gmail.com>
2018-12-11 11:48:30 -0800

    add mentor_id/mentee_id column

--- db/migrate/20181113213510_add_foreign_key_to_pairings.rb
+++ db/migrate/20181113213510_add_foreign_key_to_pairings.rb
@@ -1,5 +1,7 @@
 class AddForeignKeyToPairings < ActiveRecord::Migration[5.2]
   def change
+    add_column :pairings, :mentor_id, :integer 
+    add_column :pairings, :mentee_id, :integer
     add_foreign_key :pairings, :users, column: :mentee_id
     add_foreign_key :pairings, :users, column: :mentor_id
   end
