0b72096dda190a154d795d11a1c4b00dc8185cb9
Alex Wong <awong42191@gmail.com>
2018-12-10 19:03:31 -0800

    changing mentor/mentee migration

--- db/migrate/20181113213510_add_foreign_key_to_pairings.rb
+++ db/migrate/20181113213510_add_foreign_key_to_pairings.rb
@@ -1,6 +1,6 @@
 class AddForeignKeyToPairings < ActiveRecord::Migration[5.2]
   def change
-    add_foreign_key :pairings, :users, column: :mentee_id
-    add_foreign_key :pairings, :users, column: :mentor_id
+    add_foreign_key :pairings, :users, column: :mentee_id, primary_key: :id
+    add_foreign_key :pairings, :users, column: :mentor_id, primary_key: :id
   end
 end
