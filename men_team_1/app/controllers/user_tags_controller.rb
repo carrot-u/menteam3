2d2717ff2641a64c13c397be801da4dffc235201
Sveta Mohan <sveta.mohan@instacart.com>
2018-12-11 12:17:49 -0800

    stash changes to update master

--- men_team_1/app/controllers/user_tags_controller.rb
+++ men_team_1/app/controllers/user_tags_controller.rb
@@ -4,7 +4,7 @@ class UserTagsController < ApplicationController
   def index
 
     @user_tags = UserTag.where(user_id: @user.id)
-    
+
   end
 
   def new
@@ -14,7 +14,7 @@ class UserTagsController < ApplicationController
 
   def create
     # @user_tags = UserTag.create(params[@user.id, :topic_id])
-    @user_tags  = UserTag.new
+    @user_tags  = UserTag.new 
     @user_tags.topic_id = params[:user_tag][:topic_id]
     @user_tags.user_id = @user.id
     @user_tags.save
