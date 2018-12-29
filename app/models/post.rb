143ddcc8a7d1ec74f13250befc1e0a43b7e55c95
Alex Wong <awong42191@gmail.com>
2018-12-28 21:21:31 -0800

    Added cards to messages but still wip

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
    ### validates :user_id, presence: true ###
 
-    validates :subject, presence: true, length: {minimum: 3, maximum: 40}
+    validates :subject, presence: true, length: {minimum: 3, maximum: 150}
     validates :body, presence: true, length: {minimum: 10, maximum: 600}
 
     belongs_to :user
